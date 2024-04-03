eb7c8a0a74cf99382cad15499ece9039a334d794
ferjms <fernandojmarquesilva@gmail.com>
2024-04-03 17:50:53 +0100

    spring boot criado

---
+++ src/main/java/com/example/TUBConnect/TubConnectApplication.java
@@ -0,0 +1,13 @@
+package com.example.TUBConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TubConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TubConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TUBConnect/TubConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TUBConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TubConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
